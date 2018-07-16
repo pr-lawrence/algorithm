63beb95105b04a13dd325d7caca51f254493a4ac
kLawrence <ultimate1352@gmail.com>
2018-07-16 17:26:27 +0900

    resolve two sum problem

---
+++ leetcode/twoSum.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("hello world")
+
+	nums := []int{2, 7, 11, 15};
+	result := twoSum(nums, 9);
+	fmt.Println(result)
+}
+
+func twoSum(nums []int, target int) []int {
+	for i := 0; i < len(nums); i++ {
+		for j := i + 1 ; j < len(nums); j++ {
+			if nums[i] + nums[j] == target {
+				return []int{i ,j}
+			}
+		}
+	}
+
+	return []int{0, 0}
+}
